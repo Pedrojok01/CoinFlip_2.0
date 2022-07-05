ecf7906f730ce4023990117bf37f78fdc29a8941
Pedrojok01 <pedrojok@pm.me>
2022-07-05 08:19:58 +0700

    Edit favicon + homepage

--- src/hooks/useEventCallback.js
+++ src/hooks/useEventCallback.js
@@ -3,6 +3,7 @@ import { useCoinFlipContract } from "./useContract";
 
 export const useEventCallback = (name, callback, deps) => {
   const contract = useCoinFlipContract();
+  // eslint-disable-next-line
   const memoizedCallback = useCallback(callback, deps);
 
   useEffect(() => {
